230b3d7441ebb06be7909adf7ef7936071465168
gmshimon <simon.rosedale99@gmail.com>
2023-06-16 01:45:06 +0800

    [DONE] Title and Code of semester mapped

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,16 @@
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import AcademicSemester from './academicSemester.model';
 import { IAcademicSemester } from './academicSemeter.interface';
+import status from 'http-status';
 
 export const createSemester = async (
   playLoad: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[playLoad.title] !== playLoad.code) {
+    throw new ApiError(status.BAD_REQUEST, 'Invalid Semester Code');
+  }
+
   const result = await AcademicSemester.create(playLoad);
   return result;
 };
